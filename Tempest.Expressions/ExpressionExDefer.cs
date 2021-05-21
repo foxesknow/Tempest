37df09128f3c4156be1ca1c10352a23f6deef4a4
Sean <seanbatten58@gmail.com>
2021-05-21 18:00:39 +0100

    Tweaks!

---
+++ Tempest.Expressions/ExpressionExDefer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+
+namespace Tempest.Expressions
+{
+    public static partial class ExpressionExDefer
+    {
+        /// <summary>
+        /// Defers running an expression until after the body has been evaluates.
+        /// This is basically a reversed try/finally block.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// try
+        /// {
+        ///     body
+        /// }
+        /// finally
+        /// {
+        ///     defer
+        /// }
+        /// </code>
+        /// </example>
+        /// <param name="deferExpression"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static Expression Defer(Expression defer, Expression body)
+        {
+            if(defer == null) throw new ArgumentNullException(nameof(defer));
+            if(body == null) throw new ArgumentNullException(nameof(body));
+
+            return Expression.TryFinally(body, defer);
+        }
+    }
+}
--- Tests.Tempest.Expressions/ExpressionExTests.DoWhile.cs
+++ Tests.Tempest.Expressions/ExpressionExTests.DoWhile.cs
@@ -16,29 +16,26 @@ namespace Tests.Tempest.Expressions
         [Test]
         public void DoWhile()
         {
-            var counter = Expression.Variable(typeof(int), "a");
-            var init = Expression.Assign(counter, Expression.Constant(0));
-
-            var predicate = Expression.LessThan(counter, Expression.Constant(10));
-
-            var doWhile= ExpressionEx.DoWhile(predicate, (b, c) => 
+            var body = ExpressionEx.Let(ExpressionEx.Variable<int>("counter"), ExpressionEx.Constants.Int.Int_0, counter =>
             {
+                var predicate = Expression.LessThan(counter, Expression.Constant(10));
+
                 return Expression.Block
                 (
-                    Expression.PreIncrementAssign(counter),
-                    Expression.PreIncrementAssign(counter),
-                    Expression.PreIncrementAssign(counter)
+                    ExpressionEx.DoWhile(predicate, (b, c) => 
+                    {
+                        return Expression.Block
+                        (
+                            Expression.PreIncrementAssign(counter),
+                            Expression.PreIncrementAssign(counter),
+                            Expression.PreIncrementAssign(counter)
+                        );
+                    }),
+                    counter
                 );
             });
 
-            var block = Expression.Block
-            (
-                new ParameterExpression[]{counter},
-                init,
-                doWhile, 
-                counter
-            );
-            var lambda = Expression.Lambda<Func<int>>(block);
+            var lambda = Expression.Lambda<Func<int>>(body);
             var func = lambda.Compile();
             var answer = func();
 
@@ -48,29 +45,26 @@ namespace Tests.Tempest.Expressions
         [Test]
         public void DoWhile_LoopsOnce()
         {
-            var counter = Expression.Variable(typeof(int), "a");
-            var init = Expression.Assign(counter, Expression.Constant(0));
-
-            var predicate = Expression.LessThan(counter, Expression.Constant(0));
-
-            var doWhile= ExpressionEx.DoWhile(predicate, (b, c) => 
+            var body = ExpressionEx.Let(ExpressionEx.Variable<int>("counter"), ExpressionEx.Constants.Int.Int_0, counter =>
             {
+                var predicate = Expression.LessThan(counter, Expression.Constant(0));
+
                 return Expression.Block
                 (
-                    Expression.PreIncrementAssign(counter),
-                    Expression.PreIncrementAssign(counter),
-                    Expression.PreIncrementAssign(counter)
+                    ExpressionEx.DoWhile(predicate, (b, c) => 
+                    {
+                        return Expression.Block
+                        (
+                            Expression.PreIncrementAssign(counter),
+                            Expression.PreIncrementAssign(counter),
+                            Expression.PreIncrementAssign(counter)
+                        );
+                    }),
+                    counter
                 );
             });
-
-            var block = Expression.Block
-            (
-                new ParameterExpression[]{counter},
-                init,
-                doWhile, 
-                counter
-            );
-            var lambda = Expression.Lambda<Func<int>>(block);
+            
+            var lambda = Expression.Lambda<Func<int>>(body);
             var func = lambda.Compile();
             var answer = func();
 
