48dd1662ef71f12d1474647c569f3912c78fb55a
Sean Batten <seanbatten58@gmail.com>
2021-11-20 10:15:33 +0000

    New assembly

---
+++ Tempest.Language/Scope.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Tempest.Language
+{
+    public class Scope
+    {
+        private readonly Scope? m_Previous;
+
+        private readonly Dictionary<string, Expression> m_Bindings = new();
+
+        public Scope()
+        {
+        }
+
+        private Scope(Scope previous)
+        {
+            m_Previous = previous;
+        }
+
+        public bool IsBound(string name)
+        {
+            if(m_Bindings.ContainsKey(name)) return true;
+            if(m_Previous is not null) return m_Previous.IsBound(name);
+
+            return false;
+        }
+
+        public Scope NewScope()
+        {
+            return new Scope(this);
+        }
+    }
+}
