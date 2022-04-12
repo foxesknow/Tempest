aa8ffc5dec0608ed3b173dce25468c24bc408d2f
Sean <seanbatten58@gmail.com>
2022-04-12 15:50:10 +0100

    Added starship operator

---
+++ Tempest.Expressions/ExpressionEx.Starship.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace Tempest.Expressions
+{
+    public static partial class ExpressionEx
+    {
+        /// <summary>
+        /// An implementation of lhs <=> rhs :
+        /// 
+        /// <!<![CDATA[
+        /// The expression evalues to:
+        ///     
+        ///      0  if lhs == rhs
+        ///     <0  if lhs < rhs
+        ///     >0  if lhs > rhs
+        /// ]]>
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static Expression Starship(Expression lhs, Expression rhs)
+        {
+            if(lhs is null) throw new ArgumentNullException(nameof(lhs));
+            if(rhs is null) throw new ArgumentNullException(nameof(rhs));
+
+            if(lhs.Type != rhs.Type) throw new ArgumentException("lhs and rhs do not have the same type");
+
+            var type = lhs.Type;
+            var comparerType = typeof(Comparer<>).MakeGenericType(type);
+            
+            var getComparer = comparerType.GetProperty("Default")!;
+            var compareMethod = getComparer.PropertyType.GetMethod("Compare")!;
+            
+            var expression = Let(Expression.Property(null, getComparer), comparer =>
+            {
+                return Expression.Call(comparer, compareMethod, lhs, rhs);
+            });
+
+            return expression;
+        }
+    }
+}
---
+++ Tests.Tempest.Expressions/ExpressionExTests.Starship.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+using Tempest.Expressions;
+
+using NUnit.Framework;
+
+namespace Tests.Tempest.Expressions
+{
+    public partial class ExpressionExTests
+    {
+        [Test]
+        public void Starship_ValueTypes()
+        {
+            var lhs = Expression.Parameter(typeof(int));
+            var rhs = Expression.Parameter(typeof(int));
+            var body = ExpressionEx.Starship(lhs, rhs);
+
+            var lambda = Expression.Lambda<Func<int, int, int>>(body, lhs, rhs);
+            var function = lambda.Compile();
+
+            Assert.That(function(1, 1), Is.EqualTo(0));
+            Assert.That(function(1, 0), Is.GreaterThan(0));
+            Assert.That(function(0, 1), Is.LessThan(0));
+        }
+
+        [Test]
+        public void Starship_ReferenceTypes()
+        {
+            var lhs = Expression.Parameter(typeof(string));
+            var rhs = Expression.Parameter(typeof(string));
+            var body = ExpressionEx.Starship(lhs, rhs);
+
+            var lambda = Expression.Lambda<Func<string, string, int>>(body, lhs, rhs);
+            var function = lambda.Compile();
+
+            Assert.That(function("hello", "hello"), Is.EqualTo(0));
+            Assert.That(function("hello", "blink"), Is.GreaterThan(0));
+            Assert.That(function("hello", "trend"), Is.LessThan(0));
+        }
+    }
+}
